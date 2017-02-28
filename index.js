c13ff29cbfa53bb589923e830bbfd529863a8252
Aatu Mikkonen <t5miaa00@students.oamk.fi>
2017-02-28 19:19:48 +0200

    changed whitespaces around

--- index.js
+++ index.js
@@ -12,57 +12,56 @@ app.use(cors());
 
 // Simple hello world route
 app.get('/', function(req, res) {
-    res.send("Hello world");
+   res.send("Hello world");
 });
 
 var posts = [
-        {
+   {
+      id: 0,
+      user: {
+         id: 1,
+         username: "dtrump",
+         profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg"
+      },
+      image: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
+      imageThumbnail: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
+      likes: 892,
+      caption: "Always winning #elections",
+      tags: ['elections'],
+      comments: [
+         {
             id: 0,
             user: {
-                id: 1,
-                username: "dtrump",
-                profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg" 
-            },                                                 
-            image: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-            imageThumbnail: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-            likes: 892, 
-            caption: "Always winning #elections",
-            tags: ['elections'],         
-            comments: [
-                {
-                    id: 0,
-                    user: {
-                        id: 2,
-                        username: "POTUS"
-                    },                    
-                    comment: "You're never going to make it don #losing",
-                    userRefs: [],
-                    tags: ["losing"]
-                },
-                {
-                    id: 1,
-                    user: {
-                        id: 3,
-                        username: "HillaryC"
-                    },                    
-                    comment: "Damn right @POTUS",
-                    userRefs: ["POTUS"],
-                    tags: []       
-                },
-            ]
-
-        }
-    ]
+               id: 2,
+               username: "POTUS"
+            },
+            comment: "You're never going to make it don #losing",
+            userRefs: [],
+            tags: ["losing"]
+         },
+         {
+            id: 1,
+            user: {
+               id: 3,
+               username: "HillaryC"
+            },
+            comment: "Damn right @POTUS",
+            userRefs: ["POTUS"],
+            tags: []
+         },
+      ]
+   }
+]
 
 app.get('/posts/relevant', function(req, res) {
-    res.json(posts);
+   res.json(posts);
 });
 
-app.get('/posts/:id', function(req, res) {    
-    res.json(posts[req.params.id]);
+app.get('/posts/:id', function(req, res) {
+   res.json(posts[req.params.id]);
 });
 
 // start listening for incoming HTTP connections
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+   console.log('Node app is running on port', app.get('port'));
 });
